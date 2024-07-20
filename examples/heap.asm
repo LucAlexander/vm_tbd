b754a746c34e453fe375f6fe514386667fc09592
Luc Alexander <lucskyalex@gmail.com>
2024-07-19 23:06:13 -0400

    added capacity argument, allows nexted heaps and individual process heaps

--- examples/heap.asm
+++ examples/heap.asm
@@ -8,13 +8,12 @@ exit:
 aloc:
 	LAR	R2	FP
 	ADD	R2	R2	#x1
-	LDW	R1	R2	#x8	; heap base
-	LDW	R0	R2	#xc	; number of bytes
-	LAR	R2	ST
-	LDW R3	R0	#0	; dereference base to find heap size
+	LDW	R1	R2	#x8	; number of bytes
+	LDW	R0	R2	#xc	; heap base
+	LDW	R2	R2	#x10	; load heap capacity
+	LDW	R3	R0	#0	; dereference base to find heap size
 	ADD	R5	R3	R1	; get new heap size
-	ADD	R4	R5	R0	; get new heap end
-	CMP	R4	R2		; if heap overlaps with stack
+	CMP	R5	R2		; if heap exceeds capacity
 	JMP	GE	exit	; return 0
 	STR	R5	R0	#0	; else write new heap size
 	ADD	R3	R3	R0	; add original heap size to heap base
@@ -25,16 +24,22 @@ aloc:
 main:
 	LDW	R0	#x4		; setup base heap size
 	STR	R0	&x200	; choose heap start address
-
+	
+	LAR	R2	ST
 	LDW	R0	#x200	; heap base
-	LDW R1	#24		; requested memory (bytes)
+	SUB	R2	R2	R0	; capacity with stack limit
+	LDW	R1	#24		; requested memory (bytes)
+	PSH	R2
 	PSH	R0
 	PSH	R1
 	JSR	NC	aloc	; heap allocation request
 	POP	R0			; returns address of new memory
 
+	LAR	R2	ST
 	LDW	R0	#x200
+	SUB	R2	R2	R0
 	LDW	R1	#1
+	PSH	R2
 	PSH	R0
 	PSH	R1
 	JSR	NC	aloc
